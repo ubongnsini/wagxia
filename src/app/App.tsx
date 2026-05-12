4ffbd23a20176bef9ea1ebeb14e9444f12a66a1c
ubongnsini <93832177+ubongnsini@users.noreply.github.com>
2026-05-12 13:48:46 +0100

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,7 +6,6 @@ import { router } from './routes';
 import { UserProvider } from './context/UserContext';
 import { SidebarProvider } from './context/SidebarContext';
 import { AuthProvider } from './context/AuthContext';
-
 export default function App() {
   return (
     <UserProvider>
