ddee8dfa3f6207e45e604a9cab5f698c31b468cd
ubongnsini <93832177+ubongnsini@users.noreply.github.com>
2026-05-12 13:39:17 +0100

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 // Block wallet extensions FIRST - before anything else
 import "@/walletBlocker";
-import { RouterProvider } from 'react-router';
+
+import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import { UserProvider } from './context/UserContext';
 import { SidebarProvider } from './context/SidebarContext';
